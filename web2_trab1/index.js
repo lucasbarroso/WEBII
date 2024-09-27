b3bcffdd25b6cac4444a64f102ef7262fb4d360e
lucasbarroso <lucas.barrooso@gmail.com>
2024-09-27 18:28:52 -0300

    done up to 4

--- web2_trab1/index.js
+++ web2_trab1/index.js
@@ -29,11 +29,40 @@ const autenticacaoMiddleware = (req, res, next) => {
     res.status(401).json({ mensagem: 'Não autorizado. Token de autenticação ausente.' });
   }
 };
-// Aplicando o middleware nesta instância do Express
-app.use(autenticacaoMiddleware);
+
 
 // Exemplo de rota protegida genérica
 app.get('/rota_protegida', (req, res) => {
   res.json({ mensagem: 'Acesso permitido à rota protegida!' });
 });
 
+// 4) Rota GET com filtros usando query params
+const produtos = [
+  { id: 1, nome: 'Camiseta', categoria: 'Roupas', preco: 29.99 },
+  { id: 2, nome: 'Tênis', categoria: 'Calçados', preco: 99.99 },
+  { id: 3, nome: 'Celular', categoria: 'Eletronicos', preco: 999.99 },
+  { id: 4, nome: 'Livro', categoria: 'Livros', preco: 19.99 },
+  { id: 5, nome: 'Fone de Ouvido', categoria: 'Eletronicos', preco: 59.99 }
+];
+
+app.get('/produtos', (req, res) => {
+  let produtosFiltrados = [...produtos];
+
+  // Filtragem por categoria
+  if (req.query.categoria) {//exemplo de uso: localhost:3000/produtos?categoria=roupas
+    produtosFiltrados = produtosFiltrados.filter(produto =>
+      produto.categoria.toLowerCase() === req.query.categoria.toLowerCase()
+    );
+  }
+  // Filtragem por preço máximo
+  if (req.query.precoMax) {
+    produtosFiltrados = produtosFiltrados.filter(produto =>
+      produto.preco <= parseFloat(req.query.precoMax)
+    );
+  }
+
+  res.json(produtosFiltrados);
+  
+  // Aplicando o middleware nesta instância do Express
+  app.use(autenticacaoMiddleware);
+});
